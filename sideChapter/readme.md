62c9395988fa52a834f46b7de5ee178dd4d12b23
guest363 <guest363@gmail.com>
2018-05-04 18:52:49 +0500

    add side Chaptery

---
+++ sideChapter/readme.md
@@ -0,0 +1,55 @@
+# Shoot-Target
+
+Компонент создает оглавление из тегов h1-h6 с возможностью быстрого перехода при клике.
+
+## Входные параметры
+
+```
+ dataToChapter: {
+       sourceID: 'IDOfElement'
+  }
+```
+## Выходные параметры
+
+```
+Отсутствуют
+```
+## Инициализация
+
+Пример:
+
+```
+<template>
+  <div>
+      <side-chapter :data='dataToChapter'></side-chapter>
+  </div>
+</template>
+
+<script>
+import sideChapter from './path/side-chapter.vue';
+
+export default {
+  name: "Name",
+  components: {
+    "side-chapter": sideChapter,
+  },
+  data: () => {
+    return {
+      dataToChapter: {
+            sourceID: 'IDOfElement'
+      }
+    };
+  },
+};
+</script>
+
+```
+## Установка
+
+Достаточно скопировать файл с расширением "vue"
+
+## Как выглядит
+На примере статьи
+
+![пример на статье](https://github.com/guest363/vue-component/blob/master/shoot-target/exemple/1.jpg)
+
---
+++ sideChapter/sideChapter.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="content">
+        <h4 id="content--header">Содержание:</h4>
+        <ul id="addingElement">
+            <li>Загрузка содержания</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'sideChapter',
+        props: ['data'],
+        data() {
+            return {};
+        },
+        methods: {
+            makeMenu: function() {
+                setTimeout(() => {
+                    // отложить до отрисовки контента
+                    const el = document.getElementById(this.data.sourceID); // контейнер с содержимым Вики страницы
+                    let allH = el.querySelectorAll("h1, h2, h3, h4, h5, h6"); // Найти все заголовки
+                    const add = document.getElementById('addingElement'); // Список ul где выводится содержимое
+                    const fragment = document.createDocumentFragment(); // Чтобы не отрисовывать каждый раз страницу
+                    // элементы списка добавляются во фрагмент а затем уже весь фрагмент в DOM
+                    allH = Array.prototype.slice.call(allH); // теперь allH - массив, так удобнее работать
+                    // в частности появляется метот reduce
+                    const addList = (item, position) => {
+                        item.setAttribute("id", `C${position}`); // присвоить заголовку в ВИКИ страничке id с нумерацией
+                        // для навигации по разделам
+                        let newLi = document.createElement("li");
+                        let link = document.createElement("a");
+                        link.title = "Навигация";
+                        link.innerHTML = `<${item.tagName}> ${item.textContent} </${
+                                item.tagName
+                              }> `; // добавть к ссылке тег и содержимое заголовка
+                        link.href = `#C${position}`; // присвоить закладку на заголовок в ВИКИ
+                        newLi.appendChild(link);
+                        fragment.appendChild(newLi);
+                    };
+    
+                    allH.reduce((acc, item) => {
+                        // Создать раздел содержимое
+                        addList(item, acc);
+                        return acc + 1;
+                    }, 0);
+                    add.innerHTML = ""; // удаляет всех потомков элемента. 
+                    add.appendChild(fragment); // загружает . 
+                }, 1000);
+            }
+        },
+        mounted() {
+            this.makeMenu();
+        }
+    
+    };
+</script>
+
+<style scoped>
+    .content {
+        width: 300px;
+        position: fixed;
+        margin-left: 350px;
+    }
+    
+    .content ul {
+        list-style-type: none;
+        text-align: left;
+        margin: 0;
+        padding: 0 0 0 10px;
+        width: 100%;
+    }
+    
+    #content--header {
+        font-size: 14px;
+        font-weight: bold;
+        padding: 0;
+    }
+    
+    >>>#addingElement h1,
+    >>>#addingElement h2,
+    >>>#addingElement h3,
+    >>>#addingElement h4,
+    >>>#addingElement h5,
+    >>>#addingElement h6 {
+        font-size: 12px;
+        font-weight: normal;
+    }
+    
+    >>>#addingElement h2 {
+        padding-left: 10px;
+    }
+    
+    >>>#addingElement h3 {
+        padding-left: 20px;
+    }
+    
+    >>>#addingElement h4 {
+        padding-left: 30px;
+    }
+    
+    >>>#addingElement h5 {
+        padding-left: 40px;
+    }
+    
+    >>>#addingElement h6 {
+        padding-left: 50px;
+    }
+    
+    >>>#addingElement a {
+        text-decoration: none;
+        color: #424c55;
+    }
+</style>
