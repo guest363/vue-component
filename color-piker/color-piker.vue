b468d92ca8311876484487e7485bbc3dedfb9f18
Oleg Moiseev <guest363@gmail.com>
2018-05-07 11:44:34 +0500

    Fixed context losing

--- color-piker/color-piker.vue
+++ color-piker/color-piker.vue
@@ -1,155 +1,153 @@
 <template>
-  <div class="color-picker-wrap">
-      <div v-for="item in data.color" v-on:click="$emit('colorPiked', whatIsSelect())">
-          <div tabindex="-1" class="color-pick" v-bind:value="item"
-          v-bind:style="{ background: item }"></div>
-        </div>
- </div>
-
+<div class="color-picker-wrap">
+    <div v-for="item in data.color" v-on:click="$emit('colorPiked', whatIsSelect())">
+        <div tabindex="-1" class="color-pick" v-bind:value="item" v-bind:style="{ background: item }"></div>
+    </div>
+</div>
 </template>
 
 <script>
 const dictionary = {
-    "black": "черный",
-    "dimgray": "тускло-серый",
-    "grey": "серый",
-    "darkgray": "темно-серый",
-    "silver": "серебристый",
-    "lightgrey": "светло-серый",
-    "gainsboro": "Гейнсборо",
-    "whitesmoke": "дымчатый",
-    "white": "белый",
-    "snow": "снежный",
-    "rosybrown": "розово-коричневый",
-    "lightcoral": "светло-коралловый",
-    "indianred": "индийский красный",
-    "brown": "коричневый",
-    "firebrick": "кирпичный",
-    "maroon": "густой красно-коричневый",
-    "darkred": "темно-красный",
-    "red": "красный",
-    "salmon": "лососевый",
-    "mistyrose": "туманно-розовый",
-    "tomato": "томатный",
-    "darksalmon": "темно-лососевый",
-    "coral": "коралловый",
-    "orangered": "оранжево-красный",
-    "lightsalmon": "светло-лососевый",
-    "sienna": "сиена (охра)",
-    "seashell": "морской раковины",
-    "saddlebrown": "кожаного седла",
-    "chocolate": "шоколадный",
-    "peachpuff": "персиковый",
-    "sandybrown": "рыжеватый",
-    "linen": "льняной",
-    "peru": "Перу",
-    "bisque": "бисквитный (светло-коричневый)",
-    "darkorange": "темно-оранжевый",
-    "antiquewhite": "древний (античный) белый",
-    "tan": "дубильной коры (желтовато-коричневый)",
-    "burlywood": "плотной древесины",
-    "blanchedalmond": "миндальный (чищеного миндаля)",
-    "navajowhite": "навахо белый",
-    "papayawhip": "побега папайи",
-    "moccasin": "мокасиновый",
-    "oldlace": "старого кружева",
-    "wheat": "пшеничный",
-    "orange": "оранжевый",
-    "floralwhite": "цветочно-белый",
-    "goldenrod": "золотисто-красный",
-    "darkgoldenrod": "темный золотисто-красный",
-    "cornsilk": "молодой кукурузы  ",
-    "gold": "золотистый",
-    "lemonchiffon": "лимонного шифона",
-    "khaki": "хаки",
-    "palegoldenrod": "бледный золотисто-красный",
-    "darkkhaki": "темный хаки",
-    "ivory": "слоновой кости",
-    "beige": "беж",
-    "lightyellow": "светло-желтый",
-    "olive": "оливковый",
-    "yellow": "желтый",
-    "olivedrab": "нежно-оливковый",
-    "yellowgreen": "желто-зеленый",
-    "darkolivegreen": "темный оливково-зеленый",
-    "greenyellow": "зелено-желтый",
-    "lawngreen": "зеленой лужайки",
-    "chartreuse": "шартрез (бледно-зеленый)",
-    "honeydew": "медовой росы",
-    "darkseagreen": "темный морской волны",
-    "lightgreen": "светло-зеленый",
-    "palegreen": "бледно-зеленый",
-    "forestgreen": "лесной зелени",
-    "limegreen": "зеленого лайма  ",
-    "darkgreen": "темно-зеленый",
-    "green": "зеленый",
-    "lime": "лайма  ",
-    "seagreen": "морской волны",
-    "mediumseagreen": "умеренный морской волны",
-    "mintcream": "мятного крема",
-    "springgreen": "весенней зелени",
-    "mediumspringgreen": "умеренный весенней зелени",
-    "mediumaquamarine": "умеренный аквамариновый",
-    "aquamarine": "аквамариновый (зеленовато-голубой)",
-    "turquoise": "бирюзовый",
-    "lightseagreen": "светлый морской волны",
-    "mediumturquoise": "умеренный бирюзовый",
-    "azure": "лазурный",
-    "paleturquoise": "бледно-бирюзовый",
-    "darkslategray": "темный аспидно-серый (темно-синевато-серый)",
-    "teal": "чирок",
-    "darkcyan": "темно-циановый",
-    "aqua": "морской волны",
-    "cyan": "циановый",
-    "lightcyan": "светло-циановый",
-    "darkturquoise": "темно-бирюзовый",
-    "cadetblue": "синий кадет",
-    "powderblue": "голубой пороши",
-    "lightblue": "светло-голубой",
-    "deepskyblue": "насыщенный небесно-голубой",
-    "skyblue": "небесно-голубой",
-    "lightskyblue": "светлый небесно-голубой",
-    "steelblue": "голубовато-стальной",
-    "aliceblue": "Алиса синяя",
-    "slategray": "аспидно-серый",
-    "lightslategray": "светлый аспидно-серый",
-    "dodgerblue": "синяя уловка",
-    "lightsteelblue": "светлый голубовато-стальной",
-    "cornflowerblue": "васильковый",
-    "royalblue": "королевский голубой",
-    "ghostwhite": "призрачно-белый",
-    "lavender": "лавандовый (бледно-лиловый)",
-    "midnightblue": "синей полночи",
-    "navy": "флотский",
-    "darkblue": "темно-синий",
-    "mediumblue": "умеренный голубой (синий)",
-    "blue": "голубой (синий)",
-    "darkslateblue": "темный аспидно-синий",
-    "slateblue": "аспидно-синий",
-    "mediumslateblue": "умеренный аспидно-синий",
-    "mediumpurple": "умеренный пурпурный",
-    "blueviolet": "сине-фиолетовый",
-    "indigo": "индиго",
-    "darkorchid": "темно-лиловый",
-    "darkviolet": "темно-фиолетовый",
-    "mediumorchid": "умеренный лиловый",
-    "thistle": "чертополоха",
-    "plum": "сливовый (темно-фиолетовый с оттенками бордового)",
-    "violet": "темно-лиловый, фиолетовый",
-    "purple": "пурпурный  ",
-    "darkmagenta": "темный фуксин",
-    "fuchsia": "фуксии",
-    "magenta": "фуксин, красная анилиновая краска",
-    "orchid": "лиловый",
-    "mediumvioletred": "умеренный лилово-красный",
-    "deeppink": "насыщенный розовый",
-    "hotpink": "яркий розовый",
-    "lavenderblush": "лавандового румянца",
-    "palevioletred": "бледный лилово-красный",
-    "crimson": "малиновый, темно-красный, кармазинный",
-    "pink": "розовый",
-    "lightpink": "светло-розовый"
-    }
+  black: "черный",
+  dimgray: "тускло-серый",
+  grey: "серый",
+  darkgray: "темно-серый",
+  silver: "серебристый",
+  lightgrey: "светло-серый",
+  gainsboro: "Гейнсборо",
+  whitesmoke: "дымчатый",
+  white: "белый",
+  snow: "снежный",
+  rosybrown: "розово-коричневый",
+  lightcoral: "светло-коралловый",
+  indianred: "индийский красный",
+  brown: "коричневый",
+  firebrick: "кирпичный",
+  maroon: "густой красно-коричневый",
+  darkred: "темно-красный",
+  red: "красный",
+  salmon: "лососевый",
+  mistyrose: "туманно-розовый",
+  tomato: "томатный",
+  darksalmon: "темно-лососевый",
+  coral: "коралловый",
+  orangered: "оранжево-красный",
+  lightsalmon: "светло-лососевый",
+  sienna: "сиена (охра)",
+  seashell: "морской раковины",
+  saddlebrown: "кожаного седла",
+  chocolate: "шоколадный",
+  peachpuff: "персиковый",
+  sandybrown: "рыжеватый",
+  linen: "льняной",
+  peru: "Перу",
+  bisque: "бисквитный (светло-коричневый)",
+  darkorange: "темно-оранжевый",
+  antiquewhite: "древний (античный) белый",
+  tan: "дубильной коры (желтовато-коричневый)",
+  burlywood: "плотной древесины",
+  blanchedalmond: "миндальный (чищеного миндаля)",
+  navajowhite: "навахо белый",
+  papayawhip: "побега папайи",
+  moccasin: "мокасиновый",
+  oldlace: "старого кружева",
+  wheat: "пшеничный",
+  orange: "оранжевый",
+  floralwhite: "цветочно-белый",
+  goldenrod: "золотисто-красный",
+  darkgoldenrod: "темный золотисто-красный",
+  cornsilk: "молодой кукурузы  ",
+  gold: "золотистый",
+  lemonchiffon: "лимонного шифона",
+  khaki: "хаки",
+  palegoldenrod: "бледный золотисто-красный",
+  darkkhaki: "темный хаки",
+  ivory: "слоновой кости",
+  beige: "беж",
+  lightyellow: "светло-желтый",
+  olive: "оливковый",
+  yellow: "желтый",
+  olivedrab: "нежно-оливковый",
+  yellowgreen: "желто-зеленый",
+  darkolivegreen: "темный оливково-зеленый",
+  greenyellow: "зелено-желтый",
+  lawngreen: "зеленой лужайки",
+  chartreuse: "шартрез (бледно-зеленый)",
+  honeydew: "медовой росы",
+  darkseagreen: "темный морской волны",
+  lightgreen: "светло-зеленый",
+  palegreen: "бледно-зеленый",
+  forestgreen: "лесной зелени",
+  limegreen: "зеленого лайма  ",
+  darkgreen: "темно-зеленый",
+  green: "зеленый",
+  lime: "лайма  ",
+  seagreen: "морской волны",
+  mediumseagreen: "умеренный морской волны",
+  mintcream: "мятного крема",
+  springgreen: "весенней зелени",
+  mediumspringgreen: "умеренный весенней зелени",
+  mediumaquamarine: "умеренный аквамариновый",
+  aquamarine: "аквамариновый (зеленовато-голубой)",
+  turquoise: "бирюзовый",
+  lightseagreen: "светлый морской волны",
+  mediumturquoise: "умеренный бирюзовый",
+  azure: "лазурный",
+  paleturquoise: "бледно-бирюзовый",
+  darkslategray: "темный аспидно-серый (темно-синевато-серый)",
+  teal: "чирок",
+  darkcyan: "темно-циановый",
+  aqua: "морской волны",
+  cyan: "циановый",
+  lightcyan: "светло-циановый",
+  darkturquoise: "темно-бирюзовый",
+  cadetblue: "синий кадет",
+  powderblue: "голубой пороши",
+  lightblue: "светло-голубой",
+  deepskyblue: "насыщенный небесно-голубой",
+  skyblue: "небесно-голубой",
+  lightskyblue: "светлый небесно-голубой",
+  steelblue: "голубовато-стальной",
+  aliceblue: "Алиса синяя",
+  slategray: "аспидно-серый",
+  lightslategray: "светлый аспидно-серый",
+  dodgerblue: "синяя уловка",
+  lightsteelblue: "светлый голубовато-стальной",
+  cornflowerblue: "васильковый",
+  royalblue: "королевский голубой",
+  ghostwhite: "призрачно-белый",
+  lavender: "лавандовый (бледно-лиловый)",
+  midnightblue: "синей полночи",
+  navy: "флотский",
+  darkblue: "темно-синий",
+  mediumblue: "умеренный голубой (синий)",
+  blue: "голубой (синий)",
+  darkslateblue: "темный аспидно-синий",
+  slateblue: "аспидно-синий",
+  mediumslateblue: "умеренный аспидно-синий",
+  mediumpurple: "умеренный пурпурный",
+  blueviolet: "сине-фиолетовый",
+  indigo: "индиго",
+  darkorchid: "темно-лиловый",
+  darkviolet: "темно-фиолетовый",
+  mediumorchid: "умеренный лиловый",
+  thistle: "чертополоха",
+  plum: "сливовый (темно-фиолетовый с оттенками бордового)",
+  violet: "темно-лиловый, фиолетовый",
+  purple: "пурпурный  ",
+  darkmagenta: "темный фуксин",
+  fuchsia: "фуксии",
+  magenta: "фуксин, красная анилиновая краска",
+  orchid: "лиловый",
+  mediumvioletred: "умеренный лилово-красный",
+  deeppink: "насыщенный розовый",
+  hotpink: "яркий розовый",
+  lavenderblush: "лавандового румянца",
+  palevioletred: "бледный лилово-красный",
+  crimson: "малиновый, темно-красный, кармазинный",
+  pink: "розовый",
+  lightpink: "светло-розовый"
+};
 const pick = {
   rootElem: "",
   click: event => {
@@ -159,7 +157,7 @@ const pick = {
       event.target.classList.add("color-pick--select");
     } else event.target.classList.remove("color-pick--select");
   },
-  whatIsSelect: () => {
+  whatIsSelect: function() {
     // Возвращает массив выбранных цветов
     return this.rootElem
       .map(elem => {
@@ -171,7 +169,7 @@ const pick = {
         return elem !== "";
       });
   },
-  makePiker: elementID => {
+  makePiker: function(elementID) {
     this.rootElem = Array.prototype.slice.call(
       document.getElementsByClassName(elementID)
     );
@@ -182,61 +180,57 @@ const pick = {
   }
 };
 const makeTooltip = {
-    showingTooltip: '',
-    make: elem => {
-      const target = elem.target;
-      const tooltipElem = document.createElement("div");
-      const st = tooltipElem.style;
-      // Приходится использовать инлайн стиль из за области видимости postcss
-      st.position = 'fixed';
-      st.padding = '5px';
-      st.margin = '0';
-      st.border = '1px solid #7d7d7d';
-      st.textAlign = 'center';
-      st.color = '#353434';
-      st.background = '#fff';
-      st.boxShadow = '1px rgba(0, 0, 0, .3)';
-      st.maxWidth = '200px';
-      st.fontSize = '12px';
-      const engProp = elem.path[0].getAttribute("value");
-      tooltipElem.innerHTML = `${dictionary[engProp]}`;
-      document.body.appendChild(tooltipElem);
-      const coords = target.getBoundingClientRect();
+  showingTooltip: "",
+  make: function(elem) {
+    const target = elem.target;
+    const tooltipElem = document.createElement("div");
+    const st = tooltipElem.style;
+    // Приходится использовать инлайн стиль из за области видимости postcss
+    st.position = "fixed";
+    st.padding = "5px";
+    st.margin = "0";
+    st.border = "1px solid #7d7d7d";
+    st.textAlign = "center";
+    st.color = "#353434";
+    st.background = "#fff";
+    st.boxShadow = "1px rgba(0, 0, 0, .3)";
+    st.maxWidth = "200px";
+    st.fontSize = "12px";
+    const engProp = elem.path[0].getAttribute("value");
+    tooltipElem.innerHTML = `${dictionary[engProp]}`;
+    document.body.appendChild(tooltipElem);
+    const coords = target.getBoundingClientRect();
 
-      let left =
-        coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
-      if (left < 0) left = 0; // не вылезать за левую границу окна
+    let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+    if (left < 0) left = 0; // не вылезать за левую границу окна
 
-      let top = coords.top - tooltipElem.offsetHeight - 10;
-      if (top < 0) {
-        // не вылезать за верхнюю границу окна
-        top = coords.top + target.offsetHeight + 5;
-      }
-
-      tooltipElem.style.left = left + "px";
-      tooltipElem.style.top = top + "px";
-
-      this.showingTooltip = tooltipElem;
-    },
-    del: elem => {
-      if (this.showingTooltip) {
-        document.body.removeChild(this.showingTooltip);
-        this.showingTooltip = null;
-      }
+    let top = coords.top - tooltipElem.offsetHeight - 10;
+    if (top < 0) {
+      // не вылезать за верхнюю границу окна
+      top = coords.top + target.offsetHeight + 5;
     }
-}
+    tooltipElem.style.left = left + "px";
+    tooltipElem.style.top = top + "px";
+    this.showingTooltip = tooltipElem;
+  },
+  del: function(elem) {
+    if (this.showingTooltip) {
+      document.body.removeChild(this.showingTooltip);
+      this.showingTooltip = null;
+    }
+  }
+};
 const colorPiker = {
   name: "color-piker",
   data: () => {
-    return {
-    };
+    return {};
   },
   computed: {},
   props: ["data"],
   methods: {
-      whatIsSelect: function (){
-         return pick.whatIsSelect();
-      }
+    whatIsSelect: () => {
+      return pick.whatIsSelect();
+    }
   },
   mounted() {
     pick.makePiker("color-pick");
@@ -287,5 +281,4 @@ export default colorPiker;
   vertical-align: middle;
   padding-top: 100%;
 }
-
 </style>
